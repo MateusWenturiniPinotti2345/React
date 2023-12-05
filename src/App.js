2d4d30484afadf4abc46d770f95a1bee3fbcd0f0
Mateus Wenturini Pinotti <mateus_pinotti@estudante.sesisenai.org.br>
2023-12-04 21:52:22 -0300

    Criação do Arquivo em React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [peso, setPeso] = useState('0');
+  const [altura, setAltura] = useState('');
+  const [resultado, setResultado] = useState(null);
+  const [mensagem, setMensagem] = useState('');
+
+  const calcImc = (event) => {
+    event.preventDefault();
+
+    if (peso === '0' || altura === '') {
+      alert('Por favor, insira um peso e altura válidos');
+    } else {
+      const alturaMetros = altura / 100;
+
+      let imc = peso / (alturaMetros * alturaMetros);
+      setResultado(imc.toFixed(1));
+
+      if (imc < 18.5) {
+        setMensagem('Abaixo do Peso');
+      } else if (imc >= 18.5 && imc <= 24.9) {
+        setMensagem('Peso Ideal (Parabéns)');
+      } else if (imc >= 25.0 && imc <= 29.9) {
+        setMensagem('Levemente Acima do Peso');
+      } else if (imc >= 30.0 && imc <= 34.9) {
+        setMensagem('Obesidade Grau I');
+      } else if (imc >= 35.0 && imc <= 39.9) {
+        setMensagem('Obesidade Grau II (Severa)');
+      } else {
+        setMensagem('Obesidade Grau III (Mórbida)');
+      }
+    }
+  };
+
+  const resetFields = () => {
+    setPeso('0');
+    setAltura('');
+    setResultado(null);
+    setMensagem('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h2 className="center">Calculadora IMC</h2>
+        <form onSubmit={calcImc}>
+          <div>
+            <label>Peso (Kg)</label>
+            <input
+              value={peso}
+              onChange={(e) => setPeso(e.target.value)}
+              type="number"
+              placeholder="Digite o peso"
+            />
+          </div>
+          <div>
+            <label>Altura (cm)</label>
+            <input
+              value={altura}
+              onChange={(e) => setAltura(e.target.value)}
+              type="number"
+              placeholder="Digite a altura"
+            />
+          </div>
+          <div>
+            <button className="btn" type="submit">
+              Calcular
+            </button>
+            <button className="btn btn-outline" type="button" onClick={resetFields}>
+              Reset
+            </button>
+          </div>
+        </form>
+        {resultado !== null && (
+          <div>
+            <h3>Seu IMC é: {resultado}</h3>
+            {mensagem && <p>Sua Classificação: {mensagem}</p>}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
